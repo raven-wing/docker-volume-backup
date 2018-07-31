178df1b1fbaf3059d235b66b8ee637b23b4c01f8
krzysztof <krzysztof@localhost.localdomain>
2018-07-31 17:48:53 +0200

    It works

--- backup.sh
+++ backup.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
-CONTAINER=$1
+STACK_NAME=$1
+BACKUP_DIR=${2:-/backup}
 
-#mkdir -p backup
+backup()
+{
+    echo  "ARGS " $@
+    local STACK_NAME_FROM=$1
+    local BACKUP_DIR=$2
+    local VOLUME_TARGET=$3
+    local BACKUP_NAME=$4
+    local TASK_ID=($(get_containers_from_stack ${STACK_NAME_FROM}))
+    local CONTAINER_ID=$(get_container_id_from_task_id $TASK_ID)
+    docker run --rm --volumes-from ${CONTAINER_ID} -v ${BACKUP_DIR}:/backup:rw ubuntu tar cf /backup/${BACKUP_NAME}_backup.tar ${VOLUME_TARGET}
+}
 
-VOLUME_MOUNT=$(docker service inspect $1 -f '{{ .Spec.TaskTemplate.ContainerSpec.Mounts }}' | awk '{print $2 " " $3}')
+get_volumes_list()
+{
+    docker service inspect $1 -f '{{range .Spec.TaskTemplate.ContainerSpec.Mounts}}{{println .Target .Source}}{{end}}'
+}
 
-docker service inspect $1 -f '{{range .Spec.TaskTemplate.ContainerSpec.Mounts}}{{println .Source .Target}}{{end}}'
-    #| awk '{print $2 " " $3}'
+get_containers_id()
+{
+    docker service inspect $1 -f '{{ .ID }}'
+}
 
-#docker run --rm --volumes-from $CONTAINER -v $(pwd):/backup ubuntu tar cvf /backup/backup.tar $2
+get_containers_from_stack()
+{
+    local STACK_NAME=$1
+    docker service ps $STACK_NAME --no-trunc --filter "desired-state=Running" -q
+}
 
+get_container_id_from_task_id()
+{
+    local TASK_ID=$1
+    docker inspect -f "{{.Status.ContainerStatus.ContainerID}}" $TASK_ID
+}
+
+export -f backup
+export -f get_containers_from_stack
+export -f get_container_id_from_task_id
+export STACK_NAME_ID=$(get_containers_id $STACK_NAME)
+export STACK_NAME
+export BACKUP_DIR
+
+get_volumes_list $STACK_NAME | xargs -l bash -c 'backup ${STACK_NAME} ${BACKUP_DIR} $0 $1'
 
 
